4504f3009fe4676823473559439606c69cbcecb1
Yanga Mphikeleli <ymphikeleli@my365-uwc.ac.za>
2020-11-06 14:12:33 +0200

    first commit

---
+++ Assignment 6/Assignment 6/Assignment 6/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Banking;
+
+namespace Assignment_6
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
